08cea56e02a539f73061b3a63dbbbd9c2aa7881f
prashanth <31927461+prashanth903@users.noreply.github.com>
2019-05-11 15:24:58 +0530

    Add files via upload

---
+++ ImdbProjectMvc3/main/jbr/springmvc/controller/HomeController.java
@@ -0,0 +1,15 @@
+package jbr.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+		@RequestMapping("/home")
+		public ModelAndView hello() {
+			ModelAndView model=new ModelAndView("home");
+			return model;
+		}
+}
---
+++ ImdbProjectMvc3/main/jbr/springmvc/service/UserService.java
@@ -0,0 +1,20 @@
+package jbr.springmvc.service;
+
+import jbr.springmvc.model.Login;
+import jbr.springmvc.model.User;
+
+public class UserService {
+public static void main(String[] args) {
+	
+}
+
+public void register(User user) {
+	// TODO Auto-generated method stub
+	
+}
+
+public User validateUser(Login login) {
+	// TODO Auto-generated method stub
+	return null;
+}
+}
